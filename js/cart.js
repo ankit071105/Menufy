3fd35e0a58ea3cc7ca6c4f41012163e38ddfc4a5
Ankit Kumar <139707943+ankit071105@users.noreply.github.com>
2024-04-03 08:13:47 +0530

    Add files via upload

---
+++ js/cart.js
@@ -0,0 +1,7 @@
+(function(i,s,o,g,r,a,m){i['SalesCloudObject']=r;i[r]=i[r]||function(){
+
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+
+            m=s.getElementsByTagName(o)[0];a.src=g;m.parentNode.insertBefore(a,m)
+
+    })(window,document,'script','https://salescloud.is/js/salescloud.js','salescloud');
